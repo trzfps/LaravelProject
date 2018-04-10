b442ec40d39d0b33f5a0faa4e7eff5c98b83b0a7
Vitor <vitor_48@hotmail.com>
2018-04-09 23:43:35 -0300

    ALteração projeto

--- database/migrations/2018_03_28_024727_create_users_table.php
+++ database/migrations/2018_03_28_024727_create_users_table.php
@@ -15,27 +15,27 @@ class CreateUsersTable extends Migration
 	 */
 	public function up()
 	{
-		// Schema::create('users', function(Blueprint $table) {
+		 Schema::create('users', function(Blueprint $table) {
 		// 	// user data
-		// 	$table->increments('id');
-		// 	$table->string('cpf', 11)->unique()->nullable();
-		// 	$table->string('name', 50);
-		// 	$table->char('phone', 11);
-		// 	$table->date('birth')->nullable();
-		// 	$table->char('gender', 1)->nullable();
-		// 	$table->text('notes')->nullable();
+		 	$table->increments('id');
+			$table->string('cpf', 11)->unique()->nullable();
+			$table->string('name', 50);
+			$table->char('phone', 11);
+		 	$table->date('birth')->nullable();
+		 	$table->char('gender', 1)->nullable();
+		 	$table->text('notes')->nullable();
 
 		// 	//auth user
-		// 	$table->string('email', 80)->unique();
-		// 	$table->string('password', 254)->nullable();
+		 	$table->string('email', 80)->unique();
+			$table->string('password', 254)->nullable();
 
 		// 	//Permission 
-		// 	$table->string('status')->default('active');
-		// 	$table->string('permission')->default('app.user');
+		 	$table->string('status')->default('active');
+		 	$table->string('permission')->default('app.user');
 
-		// 	$table->rememberToken();
-		// 	$table->timestamps();
-		// 	$table->softDeletes();
+			$table->rememberToken();
+		 	$table->timestamps();
+		 	$table->softDeletes();
 			
 		});
 	}
@@ -47,9 +47,9 @@ class CreateUsersTable extends Migration
 	 */
 	public function down()
 	{
-		// Schema::table('users', function(Blueprint $table) {
+		 Schema::table('users', function(Blueprint $table) {
 
-		// });
-		// Schema::drop('users');
+		 });
+		 Schema::drop('users');
 	}
 }
--- database/migrations/2018_03_28_030709_create_user_socials_table.php
+++ database/migrations/2018_03_28_030709_create_user_socials_table.php
@@ -13,21 +13,21 @@ class CreateUserSocialsTable extends Migration
      */
     public function up()
     {
-        // Schema::create('user_socials', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->integer('user_id'->unsined();
-        //     $table->string('social_network');
-        //     $table->string('social_id');
-        //     $table->string('social_email');
-        //     $table->string('social_avatar');
+        Schema::create('user_socials', function (Blueprint $table) {
+        $table->increments('id');
+        $table->integer('user_id'->unsined();
+        $table->string('social_network');
+        $table->string('social_id');
+        $table->string('social_email');
+        $table->string('social_avatar');
 
 
 
-        //     $table->timestamps();
+            $table->timestamps();
 
-        //     $table->foreign('user_id')->references('id')->on('users');
-        //     $table->foreign('social_email')->references('email')->on('users');
-        // });
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('social_email')->references('email')->on('users');
+        });
     }
 
     /**
@@ -37,11 +37,11 @@ class CreateUserSocialsTable extends Migration
      */
     public function down()
     {
-        // Schema::table('user_socials', function(Blueprint $table){
-        //     $table->dropForeign('user_socials_user_id_foreign');
-        //     $table->dropForeign('user_socials_social_email_foreign');
+         Schema::table('user_socials', function(Blueprint $table){
+            $table->dropForeign('user_socials_user_id_foreign');
+            $table->dropForeign('user_socials_social_email_foreign');
 
-        // });
-        // Schema::dropIfExists('user_socials');
+         });
+        Schema::dropIfExists('user_socials');
     }
 }
